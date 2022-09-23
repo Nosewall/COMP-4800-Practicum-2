3ab8c2907073c54c59b6d040d358beaeb6a2e517
Jonathan Paugh <j.d.paugh@gmail.com>
2022-09-23 16:31:27 -0700

    feat: restructure project packages
    change color names

--- silver-servers-companion/app/src/main/java/com/silverservers/app/MainActivity.java
+++ silver-servers-companion/app/src/main/java/com/silverservers/app/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.silverserverscompanion;
+package com.silverservers.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.silverserverscompanion.R;
 
 public class MainActivity extends AppCompatActivity {
 
