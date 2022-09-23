c45be882f5d8becd5e3741e74d2019ef5e8684f4
Jonathan Paugh <j.d.paugh@gmail.com>
2022-09-23 16:34:50 -0700

    refactor: update app namespace

--- silver-servers-companion/app/src/main/java/com/silverservers/app/MainActivity.java
+++ silver-servers-companion/app/src/main/java/com/silverservers/app/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.silverserverscompanion.R;
+import com.silverservers.companion.R;
 
 public class MainActivity extends AppCompatActivity {
 
