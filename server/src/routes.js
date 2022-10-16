eb94ca529ca6b81d97c4e041f430ba2ed0a9f871
Jonathan Paugh <j.d.paugh@gmail.com>
2022-10-16 13:42:22 -0700

    feat: add empty routes based on api spec

--- server/src/routes.js
+++ server/src/routes.js
@@ -9,6 +9,12 @@ export default function register(app) {
   app.use(express.static("./public"));
   app.get("/", root);
   app.get("/api", api);
+  app.get("/geofence-data", geofenceData);
+  app.post("/login", login);
+  app.post("/extend-session", extendSession);
+  app.post("/update-location", updateLocation);
+  app.post("/geofence-enter", geofenceEnter);
+  app.post("/geofence-exit", geofenceExit);
 }
 
 function root(req, res) {
@@ -22,4 +28,34 @@ async function api(req, res) {
   const markdown = await read(`./private/doc/api.md`);
   const html = await marked.parse(markdown);
   res.send(html);
+}
+
+function login(req, res) {
+  console.log(login);
+  res.end();
+}
+
+function geofenceData(req, res) {
+  console.log(geofenceData);
+  res.end();
+}
+
+function extendSession(req, res) {
+  console.log(extendSession);
+  res.end();
+}
+
+function updateLocation(req, res) {
+  console.log(updateLocation);
+  res.end();
+}
+
+function geofenceEnter(req, res) {
+  console.log(geofenceEnter);
+  res.end();
+}
+
+function geofenceExit(req, res) {
+  console.log(geofenceExit);
+  res.end();
 }
