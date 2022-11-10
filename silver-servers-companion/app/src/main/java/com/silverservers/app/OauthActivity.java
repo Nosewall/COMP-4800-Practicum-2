0cc164bf00478b5de762161198210170bc44a09e
Nolan Nordwall <NolanNordwall@gmail.com>
2022-11-10 00:08:20 -0800

    Just set up some scalfolding. Initial commit.

--- silver-servers-companion/app/src/main/java/com/silverservers/app/OauthActivity.java
+++ silver-servers-companion/app/src/main/java/com/silverservers/app/OauthActivity.java
@@ -3,6 +3,7 @@ package com.silverservers.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.silverservers.companion.R;
 
@@ -12,5 +13,9 @@ public class OauthActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oauth);
+    }
+
+    public void tryOAuth(View view){
+
     }
 }
